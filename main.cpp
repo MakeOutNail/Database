0b0fd134074cb8a8abf0b26f054674082d96baab
MakeOutNail <MakeOutNail@example.com>
2026-08-28 20:54:59 +0200

    feat(shell): Add an interactive command loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+     do{
+         std::string input{};
+
+         std::cout << "minidb> ";
+
+         // Flushing the buffer
+         std::cout << std::flush;
+
+         if (!std::getline(std::cin, input)) {
+             // EOF - End of File (CTRL + D)
+             break;
+         }
+
+         if (input == ".exit") {
+             break; // Proper local variable cleanup
+         }
+
+
+         std::cout << "Unrecognized command: " << input << "\n";
+
+
+    } while (true);
+
+
     return 0;
 }
